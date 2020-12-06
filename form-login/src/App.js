24aa1cc03983b421cb72e5cecdd74fd83132a772
thiagonmiziara <thiagonmiziara@gmail.com>
2020-12-06 14:25:55 -0300

    novo curso de react js

---
+++ form-login/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+import FormularioCadastro from './components/FormularioCadstro/FormularioCadastro';
+
+function App() {
+  return (
+   <>
+     <h1>Formulário de Cadastro</h1>
+     <FormularioCadastro/>
+   </>
+  );
+}
+
+export default App;
