058198c444dd1fe81bccba668697160fc1b8d9df
thiagonmiziara <thiagonmiziara@gmail.com>
2020-12-07 20:02:09 -0300

    refatoranto trocando o swhit por um array

--- form-login/src/components/FormularioCadstro/FormularioCadastro.jsx
+++ form-login/src/components/FormularioCadstro/FormularioCadastro.jsx
@@ -2,28 +2,21 @@ import React, { useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuarios";
 import DadosEntrega from "./DadosEntrega";
-import { Typography } from "@material-ui/core";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
-  
-  function proximo(){
-    setEtapaAtual(etapaAtual+1);
+
+  const formulario = [
+    <DadosUsuario aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={aoEnviar} />,
+  ];
+
+  function proximo() {
+    setEtapaAtual(etapaAtual + 1);
   }
 
-  function formularioAtual(etapa) {
-    switch (etapa) {
-      case 0:
-        return <DadosUsuario aoEnviar={proximo}/>;
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />;
-      case 2:
-        return <DadosEntrega aoEnviar={aoEnviar}/>;
-      default:
-        return <Typography>Erro ao selecionar Formulário</Typography>;
-    }
-  }
-  return <>{formularioAtual(etapaAtual)}</>;
+  return <>{formulario[etapaAtual]}</>;
 }
 
 export default FormularioCadastro;
