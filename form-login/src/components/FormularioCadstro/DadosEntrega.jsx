47a6b91917d1e68410c8a33486acbbcfca2c677e
thiagonmiziara <thiagonmiziara@gmail.com>
2020-12-07 21:29:04 -0300

    adiciondo o useStade nos formularios

--- form-login/src/components/FormularioCadstro/DadosEntrega.jsx
+++ form-login/src/components/FormularioCadstro/DadosEntrega.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
-function DadosEntrega() {
+function DadosEntrega({ aoEnviar }) {
+  const [cep, setCep] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [numero, setNumero] = useState("");
+  const [estado, setEstado] = useState("");
+  const [cidade, setCidade] = useState("");
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({ cep, endereco, numero, estado, cidade });
+      }}
+    >
       <TextField
+        value={cep}
+        onChange={(event) => {
+          setCep(event.target.value);
+        }}
         id="cep"
         label="CEP"
         type="number"
@@ -12,6 +27,10 @@ function DadosEntrega() {
       />
 
       <TextField
+        value={endereco}
+        onChange={(event) => {
+          setEndereco(event.target.value);
+        }}
         id="endereco"
         label="Endereço"
         type="text"
@@ -21,6 +40,10 @@ function DadosEntrega() {
       />
 
       <TextField
+        value={numero}
+        onChange={(event) => {
+          setNumero(event.target.value);
+        }}
         id="numero"
         label="Numero"
         type="number"
@@ -29,6 +52,10 @@ function DadosEntrega() {
       />
 
       <TextField
+        value={estado}
+        onChange={(event) => {
+          setEstado(event.target.value);
+        }}
         id="estado"
         label="Estado"
         type="text"
@@ -37,6 +64,10 @@ function DadosEntrega() {
       />
 
       <TextField
+        value={cidade}
+        onChange={(event) => {
+          setCidade(event.target.value);
+        }}
         id="cidade"
         label="Cidade"
         type="text"
--- form-login/src/components/FormularioCadstro/DadosUsuarios.jsx
+++ form-login/src/components/FormularioCadstro/DadosUsuarios.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
-function DadosUsuario({aoEnviar}) {
-  return (
-    <form 
-    onSubmit={(event)=>{
-      event.preventDefault();
-      aoEnviar();
-    }}>
+function DadosUsuario({ aoEnviar }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({email,senha});
+      }}
+    >
       <TextField
+        value={email}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
         id="email"
         label="email"
         type="email"
@@ -19,6 +26,10 @@ function DadosUsuario({aoEnviar}) {
         variant="outlined"
       />
       <TextField
+        value={senha}
+        onChange={(event) => {
+          setSenha(event.target.value);
+        }}
         id="senha"
         label="senha"
         type="password"
